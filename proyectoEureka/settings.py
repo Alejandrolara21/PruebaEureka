a7d423c44dc5d3f5d38629a8ce78c7e84e1baee7
Alejandrolara21 <alejandroguacanemelara@gmail.com>
2021-12-07 13:47:55 -0500

    Añadiendo CORS

--- proyectoEureka/settings.py
+++ proyectoEureka/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'corsheaders',
     # Aplicaciones locales
     'apps.usuarios',
     'apps.categorias',
@@ -53,6 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ORIGIN_ALLOW_ALL=True
+CORS_ORIGIN_WHITELIST = [
+    'https://localhost:8000'
 ]
 
 ROOT_URLCONF = 'proyectoEureka.urls'
